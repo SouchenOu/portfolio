933cfc7cc2999a2c10eaa3dacdca339ddd699b05
SouchenOu <soukainaouchenuai@gmail.com>
2024-10-18 05:59:31 -0400

    start working on my new portfolio

---
+++ my-app/app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <main>
+      <div className="bg-black-100 flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
+        <h1>souchen</h1>
+     
+     </div>
+
+    </main>
+    
+  );
+}
