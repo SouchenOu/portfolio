4d2fb54729fb733a462978a55da4e2318efba14c
SouchenOu <soukainaouchenuai@gmail.com>
2024-10-21 07:34:21 -0400

    start working for the header part

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,14 @@
+import { Main } from "@/components/Main";
 
 export default function Home() {
   return (
     <main>
-      <div className="bg-black-100 flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
-        <h1>souchen</h1>
+      <div className="relative bg-black-100 flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
+        <div className="max-w-7xl w-full">
+          <Main/>
+
+
+        </div>
      
      </div>
 
